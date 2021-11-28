453513b4cfa2ef6947ac5a5e1e6f0083a2097c23
Prem-Thakkar <premthakkar@outlook.com>
2021-11-29 02:00:19 +0530

    Changed design

--- resources/views/courses/list.blade.php
+++ resources/views/courses/list.blade.php
@@ -19,18 +19,34 @@
 <div class="card">
     <div class="card-body row">
         @forelse($usercourses as $usercourse)
-        <div class="card col-md-2">
+            <div class="col-md-6">
+                <a href="{{ route('courses.list.detail',['id'=>$usercourse->course->id]) }}" class="text-dark">
+                    <div class="card shadow">
+                        <div class="card-body">
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <div class="row">
+                                        <div class="col-md-4">
+                                            <img  style="height:60px;" class="img-fluid img-thumbnail col-md-6" src="{{asset('storage/upload/course-images/'.$usercourse->course->image)}}" alt="Card image cap">
+                                        </div>
+                                        <div class="col-md-6">
+                                            <h5>{{$usercourse->course->title}}</h5>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </a>
+            </div>
+
+        <!-- <div class="card col-md-2">
             <img style="height: 25%;" class="img-fluid img-thumbnail card-img-top" src="{{asset('storage/upload/course-images/'.$usercourse->course->image)}}" alt="Card image cap">
             <div class="card-body">
                 <h5 class="card-title">{{$usercourse->course->title}}</h5>
-                <div style="  text-overflow: ellipsis;
-  word-wrap: break-word;
-  overflow: hidden;
-  max-height: 10em;
-  line-height: 1.8em;" class="card-text">{!! $usercourse->course->description !!}</div>
                 <a href="{{ route('courses.list.detail',['id'=>$usercourse->course->id]) }}" class="btn btn-primary">See in detail</a>
             </div>
-        </div>
+        </div> -->
         @empty
         <h2>No courses found.</h2>
         @endforelse
